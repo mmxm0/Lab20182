89b1223294459f5c1ba39064c29af16ed9f3cd66
mmxm0 <yeterorish@hotmail.com>
2018-10-19 09:00:42 -0300

    func que descobre o numero diferente

--- quadradoQuaseMagico.py
+++ quadradoQuaseMagico.py
@@ -1,9 +1,34 @@
+def somaCol(matriz):
+    soma=0
+    colunas=[]
+    for i in range(len(matriz)):
+        for j in range(len(matriz)):
+            soma+=matriz[j][i]
+        colunas.append(soma)
+        soma=0
+    return colunas
+def descobre(l):
+    dif = 0 #index do numero que eu quero
+    for i in range(len(l)-1):
+        if l.count(l[i])<l.count(l[i+1]):
+            dif = i
+            break
+    return dif
+
 n = int(input())
 cont = 0
 quadrado = [[]]*n
+somaLinhas = []
+
+dic = {}
 while cont<n:
+    
     linha = input().split()
     linha = [int(x) for x in linha]
+    somaLinhas.append(sum(linha))
     quadrado[cont]=linha
     cont+=1
-print(quadrado)
+
+somaColunas = somaCol(quadrado)
+
+print(quadrado, somaLinhas, descobre(somaLinhas))
